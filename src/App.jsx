c44d9d458eed6740e8677d79f981e74e814734ab
Chimukupo Goma <Chimukupo77@gmail.com>
2025-05-24 01:18:47 +0200

    App.jsx Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,31 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 const Card = ({ title }) => {
+  const [hasLiked, setHasLiked] = useState(false);
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    console.log(`${title} has been liked: ${hasLiked}`);
+  });
+  
   return (
-    <div className="card">
-      <h2>{title}</h2>
+    <div className="card" onClick={() => setCount(count + 1)}>
+      <h2>{title} <br/> {count}</h2>
+      <button onClick={() => setHasLiked(!hasLiked)}>
+        {hasLiked ? 'Liked!' : 'Like'}      
+      </button>
     </div>
   )
 }
 
 const App = () => {  
-  // const [ variableName, setVariable ] = useState();
-  const [hasLiked, setHasLiked] = useState(initialState:false);
-
   return (
     <div className="card-container">
-      <Card title="Superman" ratings={5} isCool={true} />
+      <Card title="Superman" rating={5} isCool={true} />
       <Card title="Fantastics Four: First Steps" />
       <Card title="F1" />
       <Card title="Sinners" />
       <Card title="Ballerina" />
     </div>
-    
   )
 }
 
