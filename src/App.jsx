3d3026c290f17af2447cab6aa2b44ab524f3fe8e
Chimukupo Goma <Chimukupo77@gmail.com>
2025-05-25 23:48:08 +0200

    API update

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,40 @@
-import { useEffect, useState } from 'react';
-import Search from './components/search'
+import { useEffect, useState } from 'react'
+import Search from './components/search.jsx'
 
-const API_BASE_URL = 'https://developer.themoviedb.org/3';
-
-const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
-
+const API_BASE_URL = 'https://api.themoviedb.org/3'
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY
 const API_OPTIONS = {
   method: 'GET',
   headers: {
     accept: 'application/json',
-    Authorization: `Bearer ${API_KEY}`
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNGFhMzgyYzM1ZDA1OTIxZjIxYWU2ZTMxZmIyZTIzYiIsIm5iZiI6MTc0ODExNTY0Ny41OTMsInN1YiI6IjY4MzIyMGJmODBmOWM5MGMzZDAzNmJjNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ujlicX-Mhw1RGJbnOF3gpf-RqWen_ExaaF1WatVG-rs'
   }
 }
 
 const App = () => {
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState('')
+  const [errorMessage, setErrorMessage] = useState('')
+
+  const fetchMovies = async () => {
+    try {
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`
+      const response = await fetch(endpoint, API_OPTIONS)
+
+      if (!response.ok) {
+        throw new Error('Failed to fetch movies')
+      }
+
+      const data = await response.json()
+      console.log(data)
+      setErrorMessage('')
+    } catch (error) {
+      console.error(`Error fetching movies: ${error}`)
+      setErrorMessage('Error fetching movies. Please try again later.')
+    }
+  }
 
   useEffect(() => {
-      
+      fetchMovies();
     }, []);
   
 
@@ -28,11 +45,14 @@ const App = () => {
       <div className='wrapper'>
       <header>
         <img src="./hero.png" alt="Hero Banner" />
-
         <h1>You Love <span className='text-gradient'>Movies</span>? We Do Too!</h1>
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       </header>
 
-      <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      <section className='all-movies'>
+        <h2>All Movies</h2>
+      </section>
+
       <h1 className='text-white'></h1>
       </div>
     </main>
