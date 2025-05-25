28286b7c2e92c4ed863805a51f1975d872527f55
Chimukupo Goma <Chimukupo77@gmail.com>
2025-05-26 00:28:29 +0200

    TMDB API Configured

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,14 @@ const API_OPTIONS = {
 const App = () => {
   const [searchTerm, setSearchTerm] = useState('')
   const [errorMessage, setErrorMessage] = useState('')
+  const [movieList, setMovieList] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  
 
   const fetchMovies = async () => {
+    setIsLoading(true)
+    setErrorMessage('')
+
     try {
       const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`
       const response = await fetch(endpoint, API_OPTIONS)
@@ -25,11 +31,20 @@ const App = () => {
       }
 
       const data = await response.json()
-      console.log(data)
-      setErrorMessage('')
+      
+      if(data.Response === 'False') {
+        setErrorMessage(data.Error || 'Failed to fetch movies')
+        setMovieList([])
+        return;
+      }        
+
+      setMovieList(data.results || [])
+
     } catch (error) {
       console.error(`Error fetching movies: ${error}`)
       setErrorMessage('Error fetching movies. Please try again later.')
+    } finally {
+      setIsLoading(true)
     }
   }
 
@@ -51,6 +66,18 @@ const App = () => {
 
       <section className='all-movies'>
         <h2>All Movies</h2>
+
+        {isLoading ? (
+          <p className='text-white'>Loading...</p>
+        ) : errorMessage ? (
+          <p className='text-red-500'>{errorMessage}</p>
+        ) : (
+          <ul>
+            {movieList.map((movie) => (
+              <p key={movie.id} className='text-white'>{movie.title}</p>
+            ))}
+          </ul>
+        )}
       </section>
 
       <h1 className='text-white'></h1>
