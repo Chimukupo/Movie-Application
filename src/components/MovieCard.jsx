b3d838dcefdc14d4ae9b7456b1d3c7fded573cae
Chimukupo Goma <Chimukupo77@gmail.com>
2025-07-06 16:14:37 +0200

    tailwindcss v4 fix

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 
-const MovieCard = ({ movie: { title, vote_average, poster_path, release_date, original_language } 
+const MovieCard = ({ movie:
+  { title, vote_average, poster_path, release_date, original_language }
 }) => {
   return (
-    <div className='movie-card'>
-      <img 
-      src={poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` : '/no-movie.png'} 
-      alt={title || 'Movie poster'} 
+    <div className="movie-card">
+      <img
+        src={poster_path ?
+          `https://image.tmdb.org/t/p/w500/${poster_path}` : '/no-movie.png'}
+        alt={title}
       />
+
+      <div className="mt-4">
+        
+      </div>
     </div>
   )
 }
-
 export default MovieCard
